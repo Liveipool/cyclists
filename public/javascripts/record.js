b42c83fe9b81d751b42d0f8ecfecd78bf8ade532
liveipooli(李为) <liveipooli@webank.com>
2019-04-25 23:09:31 +0800

    ADD：增加create a record的成功和错误提示

--- public/javascripts/record.js
+++ public/javascripts/record.js
@@ -33,17 +33,29 @@ $(document).ready(function() {
         }
         console.log("record: ", record);
 
-        $.ajax({
-            //post 'POST' require to the server, route is /record
-            type: 'POST',
-            url: '/record',
-            data: record,
-            success: function(data) {
-                // 按理说应该是只更新右边的数据、清空左边的表格而不是刷新整个页面，后面有空再优化
-                document.getElementById('distance').innerHTML ="";
-                location.reload();
-            }
-        });
+        let valid = true;
+        if (record.weight === '') valid = false;
+        if (record.date === '') valid = false;
+        if (record.startTime === '') valid = false;
+        if (record.endTime === '') valid = false;
+
+        if (valid) {
+            // 说明所有基本信息都不为空，可以post
+            $.ajax({
+                //post 'POST' require to the server, route is /record
+                type: 'POST',
+                url: '/record',
+                data: record,
+                success: function(data) {
+                    // 按理说应该是只更新右边的数据、清空左边的表格而不是刷新整个页面，后面有空再优化
+                    document.getElementById('distance').innerHTML ="";
+                    alert('Success!')
+                    location.reload();
+                }
+            });
+        } else {
+            alert('Please enter all infomation!')
+        }
         return false;
     })
 })
